a3a3cb3d79eafd794d703f21c25911d9f5b91034
hangarame <hanjay23@naver.com>
2025-01-07 17:33:04 +0900

    생성자, getter/setter 추가

--- src/com/fnsms/instructor/Instructor.java
+++ src/com/fnsms/instructor/Instructor.java
@@ -4,11 +4,34 @@ import java.util.ArrayList;
 
 import com.fnsms.attendance.Attendance;
 import com.fnsms.emp.Emp;
-import com.fnsms.attendance.Attendance;
+
 
 
 public class Instructor extends Emp {
+	
 	private ArrayList<Attendance> Attendance;
+
+	public Instructor(String empNo, String role, ArrayList<Attendance> attendance) {
+		super(empNo, role);
+		Attendance = attendance;
+	}
+
+	public ArrayList<Attendance> getAttendance() {
+		return Attendance;
+	}
+
+	public void setAttendance(ArrayList<Attendance> attendance) {
+		Attendance = attendance;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Instructor [Attendance=");
+		builder.append(Attendance);
+		builder.append("]");
+		return builder.toString();
+	}
 	
 	
 
