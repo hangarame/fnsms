d1a29af4fe404a033aab6b87389c039fb498fb47
dnjs0 <heatherray@naver.com>
2025-01-07 15:49:34 +0900

    ticket.java
    getter setter 추가해봤습니다

--- src/com/fnsms/ticket/Ticket.java
+++ src/com/fnsms/ticket/Ticket.java
@@ -12,5 +12,116 @@ public class Ticket {
 	private int classTimes;
 	private int price;
 	
+	
+	public Ticket(int ticketNo, int ticketClassify, int ticketUseDays, Calendar regDate, Calendar sellEndDate,
+			int classTimes, int price) {
+		super();
+		this.ticketNo = ticketNo;
+		this.ticketClassify = ticketClassify;
+		this.ticketUseDays = ticketUseDays;
+		this.regDate = regDate;
+		this.sellEndDate = sellEndDate;
+		this.classTimes = classTimes;
+		this.price = price;
+	}
+
+
+	public int getTicketNo() {
+		return ticketNo;
+	}
+
+
+	public void setTicketNo(int ticketNo) {
+		this.ticketNo = ticketNo;
+	}
+
+
+	public int getTicketClassify() {
+		return ticketClassify;
+	}
+
+
+	public void setTicketClassify(int ticketClassify) {
+		this.ticketClassify = ticketClassify;
+	}
+
+
+	public int getTicketUseDays() {
+		return ticketUseDays;
+	}
+
+
+	public void setTicketUseDays(int ticketUseDays) {
+		this.ticketUseDays = ticketUseDays;
+	}
+
+
+	public Calendar getRegDate() {
+		return regDate;
+	}
+
+
+	public void setRegDate(Calendar regDate) {
+		this.regDate = regDate;
+	}
+
+
+	public Calendar getSellEndDate() {
+		return sellEndDate;
+	}
+
+
+	public void setSellEndDate(Calendar sellEndDate) {
+		this.sellEndDate = sellEndDate;
+	}
+
+
+	public int getClassTimes() {
+		return classTimes;
+	}
+
+
+	public void setClassTimes(int classTimes) {
+		this.classTimes = classTimes;
+	}
+
+
+	public int getPrice() {
+		return price;
+	}
+
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Ticket [ticketNo=");
+		builder.append(ticketNo);
+		builder.append(", ticketClassify=");
+		builder.append(ticketClassify);
+		builder.append(", ticketUseDays=");
+		builder.append(ticketUseDays);
+		builder.append(", regDate=");
+		builder.append(regDate);
+		builder.append(", sellEndDate=");
+		builder.append(sellEndDate);
+		builder.append(", classTimes=");
+		builder.append(classTimes);
+		builder.append(", price=");
+		builder.append(price);
+		builder.append("]");
+		return builder.toString();
+	}
+	
+	
+	
+	
+	
+	
+	
 
 }
