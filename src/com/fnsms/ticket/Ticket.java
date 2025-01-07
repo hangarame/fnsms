92ffafc0daf0efffa4aace6bb75744a26ccfa0e5
dnjs0 <heatherray@naver.com>
2025-01-07 15:24:27 +0900

    test1

---
+++ src/com/fnsms/ticket/Ticket.java
@@ -0,0 +1,16 @@
+package com.fnsms.ticket;
+
+import java.util.Calendar;
+
+public class Ticket {
+	
+	private int ticketNo;
+	private int ticketClassify;
+	private int ticketUseDays;
+	private Calendar regDate;
+	private Calendar sellEndDate;
+	private int classTimes;
+	private int price;
+	
+
+}
