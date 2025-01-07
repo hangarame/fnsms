68b35529a6a78fa98de30e16bb7629470b39329a
hangarame <hanjay23@naver.com>
2025-01-07 17:21:36 +0900

    Emp getter/setter 추가

--- src/com/fnsms/emp/Emp.java
+++ src/com/fnsms/emp/Emp.java
@@ -3,6 +3,39 @@ package com.fnsms.emp;
 import com.fnsms.user.User;
 
 public class Emp extends User{
-	 private String empNo;
-	 private String role;
+	private String empNo;
+	private String role;
+	 
+	public Emp(String empNo, String role) {
+		this.empNo = empNo;
+		this.role = role;
+	}
+
+	public String getEmpNo() {
+		return empNo;
+	}
+
+	public void setEmpNo(String empNo) {
+		this.empNo = empNo;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Emp [empNo=");
+		builder.append(empNo);
+		builder.append(", role=");
+		builder.append(role);
+		builder.append("]");
+		return builder.toString();
+	}
+	 
 }
--- src/com/fnsms/instructor/Instructor.java
+++ src/com/fnsms/instructor/Instructor.java
@@ -3,6 +3,8 @@ package com.fnsms.instructor;
 import java.util.ArrayList;
 
 import com.fnsms.emp.Emp;
+import com.fnsms.attendance.Attendance;
+
 
 public class Instructor extends Emp {
 	private ArrayList<Attendance> Attendance;
