0cdecc3fe43b7d31404f74781d22fc831fd38baa
zionpaul1047 <zionpaul1047@gmail.com>
2025-01-08 09:19:50 +0900

    1

--- src/com/fnsms/member/MemberService.java
+++ src/com/fnsms/member/MemberService.java
@@ -2,15 +2,15 @@ package com.fnsms.member;
 
 import java.util.ArrayList;
 
-import com.fnsms.reservation.Reservation;
+
 import com.fnsms.user.UserService;
 
 public class MemberService extends UserService {
 
 	// 예약 조회
-	public ArrayList<Reservation> inquiryReservInfo() {
-		return null;
-	}
+//	public ArrayList<Reservation> inquiryReservInfo() {
+//		return null;
+//	}
 
 	public boolean haveUseTowelTicket() {
 		return false;
