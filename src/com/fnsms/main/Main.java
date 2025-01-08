dfa73607e5a932e648d80cb60c567bad464342d8
zionpaul1047 <zionpaul1047@gmail.com>
2025-01-08 11:00:42 +0900

    .

--- src/com/fnsms/main/Main.java
+++ src/com/fnsms/main/Main.java
@@ -1,16 +1,16 @@
 package com.fnsms.main;
 
+import java.lang.reflect.Member;
+
 import com.fnsms.view.CalendarView;
 import com.fnsms.view.CommonView;
+import com.fnsms.view.MemberView;
 
 public class Main {
 	public static void main(String[] args) {
 		CommonView.printLogin();
 		CommonView.printLoginFailed();
 		CommonView.printLogout();
-//
-		new CalendarView().start();
 		
-
 	}
 }
