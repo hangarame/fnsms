bbd82beba02dc6110897bc38a6c5dde768507cca
zionpaul1047 <zionpaul1047@gmail.com>
2025-01-08 11:26:04 +0900

    .

--- src/com/fnsms/main/Main.java
+++ src/com/fnsms/main/Main.java
@@ -2,15 +2,12 @@ package com.fnsms.main;
 
 import com.fnsms.view.CalendarView;
 import com.fnsms.view.CommonView;
+import com.fnsms.view.Header;
+import com.fnsms.view.InstructorView;
 
 public class Main {
 	public static void main(String[] args) {
-		CommonView.printLoginSelct();
-		CommonView.printLogin();
-//		CommonView.printLoginFailed();
-//		CommonView.printLogout();
-//
-		new CalendarView().start();
+		new Header().Logo();
 		
 
 	}
--- src/com/fnsms/view/Header.java
+++ src/com/fnsms/view/Header.java
@@ -2,30 +2,7 @@ package com.fnsms.view;
 
 import java.util.Calendar;
 
-public class Header {
-	
-	public static final String tBLACK = "\033[30m"; // 검정
-	public static final String tRED = "\033[31m"; // 빨강
-	public static final String tGREEN = "\033[32m"; // 초록
-	public static final String tYELLOW = "\033[33m"; // 노랑
-	public static final String tBLUE = "\033[34m"; // 파랑
-	public static final String tMAGENTA = "\033[35m"; // 자주색
-	public static final String tCYAN = "\033[36m"; // 청록색
-	public static final String tWHITE = "\033[37m"; // 흰색
-	// 배경 색상
-	public static final String bRED = "\033[41m"; // 빨강 배경
-	public static final String bGREEN = "\033[42m"; // 초록 배경
-	public static final String bYELLOW = "\033[43m"; // 노란 배경
-	public static final String bBULE = "\033[44m"; // 파랑 배경
-	public static final String bMAGENTA = "\033[45m"; // 자주색 배경
-	public static final String bCAYN = "\033[46m"; // 청록색 배경
-	public static final String bWHITE = "\033[47m"; // 흰색 배경
-	// 특수효과
-	public static final String BOLD = "\033[1m"; // 굵게
-	public static final String UNDERLINE = "\033[4m"; // 밑줄
-
-	// 효과 초기화
-	public static final String RESET = "\033[0m";
+public class Header implements ConsoleColor {
 	
 	
 	public void Logo() {
