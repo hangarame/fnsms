45795186812eb400155cc3cbfea833ad51f47069
hangarame <hanjay23@naver.com>
2025-01-09 01:13:07 +0900

    2-1 이용권조회 관련 소스 등록

--- src/com/fnsms/main/Main.java
+++ src/com/fnsms/main/Main.java
@@ -32,7 +32,6 @@ public class Main {
 		UserService.logIn();
 
 		
-		
 
 	}
 }
--- src/com/fnsms/member/MemberService.java
+++ src/com/fnsms/member/MemberService.java
@@ -3,6 +3,7 @@ package com.fnsms.member;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Comparator;
+import java.util.Scanner;
 
 import com.fnsms.dao.MemberDAO;
 import com.fnsms.dao.ReservationDAO;
@@ -55,6 +56,7 @@ public class MemberService extends UserService {
 		 return false;
 	}
 	
+	//운동복/수건 이용권이 있는지
 	public boolean haveUseTowelTicketUse() {
 		 for(TicketRegistration reg : TicketRegistrationDAO.getTicketRegList(this.getMember().getMemberNo()) ) {
 			 if(reg.getTicket().startsWith("운동복")) {
@@ -67,11 +69,11 @@ public class MemberService extends UserService {
 	
 	
 	//소유한 이용권 중 가장 가까운 만료일 반환 
-	public int getNearExpiredate(ArrayList<TicketRegistration> regList) {
+	public long getNearExpiredate(ArrayList<TicketRegistration> regList) {
 		if(regList.size() < 1 ) {
 			return -1;
 		} else {
-			int min = 0;
+			long min = 0;
 			
 			for(TicketRegistration reg : regList) {
 				if(getTicketExpireDateGap(reg) < min)
@@ -83,8 +85,29 @@ public class MemberService extends UserService {
 
 	}
 	
-	//현재로부터 티켓의 만료일 D-Day
-	public int getTicketExpireDateGap(TicketRegistration reg) {
+	//이용권의 총 이용기간
+	public long getTicketTotalDays(TicketRegistration reg) {
+		Calendar endDate = Calendar.getInstance();
+
+		int endYyyy = reg.getEndDate().get(Calendar.YEAR);
+		int endMm = reg.getEndDate().get(Calendar.MONTH);
+		int endDd = reg.getEndDate().get(Calendar.DATE);
+		
+		endDate.set(endYyyy, endMm, endDd, 0, 0, 0);
+		
+		Calendar startDate = Calendar.getInstance();
+		int startYyyy = reg.getStartDate().get(Calendar.YEAR);
+		int startMm = reg.getStartDate().get(Calendar.MONTH);
+		int startDd = reg.getStartDate().get(Calendar.DATE);
+		
+		startDate.set(startYyyy, startMm, startDd, 0, 0, 0);
+		
+		return (endDate.getTimeInMillis() - startDate.getTimeInMillis()) / 1000 / 60 / 60 / 24;
+	}
+	
+	
+	//현재로부터 이용권의 만료일 D-Day
+	public long getTicketExpireDateGap(TicketRegistration reg) {
 		
 		Calendar baseDate = Calendar.getInstance();
 
@@ -100,8 +123,9 @@ public class MemberService extends UserService {
 		currentDate.set(Calendar.SECOND, 0);
 
 
-		return (int)((baseDate.getTimeInMillis() - currentDate.getTimeInMillis()) / 1000 / 60 / 60 / 24);
+		return (baseDate.getTimeInMillis() - currentDate.getTimeInMillis()) / 1000 / 60 / 60 / 24;
 	}
+	
 	
 	
 	// 기간이 유효한 이용권 등록 가져오기(현재일이 시작일과 종료일 기간이 아니면 해당되지 않음)
@@ -158,18 +182,18 @@ public class MemberService extends UserService {
 		
 		//예약횟수
 		int reservCount = getTicketUsedIning(ticketReg);
-		
-		
-		
-		
+
 		return useIning - reservCount;
 		
 	}
 	
 	
-	
 	//로그인 후 회원 메인화면
 	public void memberMainMenu() {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		
 		//회원의 이용중인 유효한 이용권
 		ArrayList<TicketRegistration> validRegList = this.getValidRegstration(this.getMember());
 		
@@ -178,6 +202,41 @@ public class MemberService extends UserService {
 				, validRegList.get(0).getTicket()
 				, this.getTicketRemainIning(validRegList.get(0))
 				, validRegList.get(0).getEndDate());
+		
+//		return validRegList.get(0);
+		
+		while(true) {
+			String cmd = scan.nextLine();
+			
+			if(cmd.equals("1")) {
+				inquiryTicketInfo(validRegList.get(0));
+				
+			} else if(cmd.equals("2")) {
+				
+			} else if(cmd.equals("E")) {
+				
+			} else {
+				
+			}
+		}
+		
+	}
+	
+	//2-1 이용권 정보 조회
+	public void inquiryTicketInfo(TicketRegistration ticketReg) {
+		//printDate(Calendar registerDate, Calendar startDate, Calendar endDate, long totalDays, long remainingDays, String name, boolean towel, String ticket, int count)
+		Calendar registerDate = ticketReg.getPurchaseDate();
+		Calendar startDate = ticketReg.getStartDate();
+		Calendar endDate = ticketReg.getEndDate();
+		long totalDays = getTicketTotalDays(ticketReg);
+		long remainingDays = getTicketExpireDateGap(ticketReg);
+		String name = this.getMember().getName();
+		boolean towel = haveUseTowelTicketUse();
+		String ticket = ticketReg.getTicket();
+		int count = getTicketRemainIning(ticketReg);
+		
+		MemberView.printDate(registerDate, startDate, endDate, totalDays, remainingDays, name, towel, ticket, count);
+		
 	}
 	
 
