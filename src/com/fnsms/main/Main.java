29905d3373155f42797d2205200133611e903585
dnjs0 <heatherray@naver.com>
2025-01-09 12:12:53 +0900

    관리자 매출관리

--- src/com/fnsms/main/Main.java
+++ src/com/fnsms/main/Main.java
@@ -2,16 +2,12 @@ package com.fnsms.main;
 
 
 import com.fnsms.view.AdministratorView;
-import com.fnsms.view.CalendarView;
-import com.fnsms.view.CommonView;
-import com.fnsms.view.InstructorClassMngView;
-import com.fnsms.view.InstructorView;
+
 
 public class Main {
 	public static void main(String[] args) {
-		AdministratorView.manageClass();
 
-
+		AdministratorView.recordSaleGraph();
 	}
 
 }
