fb6eda65638eca5d8326ff700de95dd0dc8ad145
dnjs0 <heatherray@naver.com>
2025-01-09 15:15:59 +0900

    empdateservice

--- src/com/fnsms/main/Main.java
+++ src/com/fnsms/main/Main.java
@@ -1,10 +1,13 @@
 package com.fnsms.main;
 
+import com.fnsms.user.UserService;
+import com.fnsms.view.InstructorClassMngView;
+
 public class Main {
 	public static void main(String[] args) {
+		InstructorClassMngView.printSelectReservationDate("홍길동","PT");
 		
-		
-		
+		UserService.logIn();
 	}
 
 }
