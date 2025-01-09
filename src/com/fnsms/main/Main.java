cab853b571cd070ff0b0d8a583330b3803ce0bf9
hangarame <hanjay23@naver.com>
2025-01-09 11:44:54 +0900

    휴회신청 작업중

--- src/com/fnsms/main/Main.java
+++ src/com/fnsms/main/Main.java
@@ -1,12 +1,25 @@
 package com.fnsms.main;
 
-import com.fnsms.view.InstructorMemberView;
+import com.fnsms.dao.AttendanceDAO;
+import com.fnsms.dao.EmpDAO;
+import com.fnsms.dao.MemberDAO;
+import com.fnsms.dao.ReservationDAO;
+import com.fnsms.dao.TicketDAO;
+import com.fnsms.dao.TicketRegistrationDAO;
+import com.fnsms.user.UserService;
 
 public class Main {
 	public static void main(String[] args) {
-		
 
-		InstructorMemberView.calculateTuition();
+		AttendanceDAO.load();
+		EmpDAO.load();
+		MemberDAO.load();
+		ReservationDAO.load();
+		TicketDAO.load();
+		TicketRegistrationDAO.load();
+		
+		UserService.logIn();
+	
 		
 
 
